c0d1791de132d8afc215ab3ae1a7f012a5d84647
Riyaz-Gazi <riyazgazi9@gmail.com>
2025-11-29 15:13:29 +0530

    jwt service added

--- src/main/java/com/codingshuttle/SpringSecurity/entity/User.java
+++ src/main/java/com/codingshuttle/SpringSecurity/entity/User.java
@@ -1,6 +1,10 @@
 package com.codingshuttle.SpringSecurity.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,6 +12,11 @@ import java.util.Collection;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/codingshuttle/SpringSecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/codingshuttle/SpringSecurity/SpringSecurityApplicationTests.java
@@ -1,13 +1,25 @@
 package com.codingshuttle.SpringSecurity;
 
+import com.codingshuttle.SpringSecurity.entity.User;
+import com.codingshuttle.SpringSecurity.service.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
+    @Autowired
+    private JwtService jwtService;
+
 	@Test
 	void contextLoads() {
+        User user = new User(2L,"riyaz@gmail.com","1234");
+        System.out.println("Token Generation");
+        String token = jwtService.generateAccessToken(user);
+        System.out.println(token);
+        Long id = jwtService.getUserIdFromToken(token);
+        System.out.println(id);
 	}
 
 }
